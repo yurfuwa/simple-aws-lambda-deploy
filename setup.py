2700ab3eb55954b2f90e1df20b3a3b7745957e65
yusuke-kamo <yusuke.kamo@dena.jp>
2018-04-24 10:28:23 +0900

    mod: setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ def main():
     
     setup(
         name='simple-aws-lambda-deploy',
-        version='0.1.0',
+        version='0.1.1',
         zip_safe=False,
+        entry_points={
+            "console_scripts": [
+                "simple-lambda-deploy=script.deploy:deploy",
+                ],
+            },
     )
 
 
