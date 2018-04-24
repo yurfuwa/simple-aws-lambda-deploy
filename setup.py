f2c913857bf7795527ea99d74f623ed360d48888
yusuke-kamo <yusuke.kamo@dena.jp>
2018-04-24 10:15:59 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+def main():
+    
+    setup(
+        name='simple-aws-lambda-deploy',
+        version='0.1.0',
+        zip_safe=False,
+    )
+
+
+if __name__ == '__main__':
+    main()
